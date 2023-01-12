19cff0890ba4226d375dcff38a07a7379e67de56
Edgar Elias <edgar_jk3@hotmail.com>
2023-01-12 20:47:10 -0300

    "Desafio concluido, TODO'S implementados com sucesso."

--- Program.cs
+++ Program.cs
@@ -1,3 +1,19 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+int delay = 2000;
+
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia("123456", "Nokia", "1111111", 128);
+nokia.Ligar();
+Thread.Sleep(delay);
+nokia.InstalarAplicativo("Whatsapp");
+Thread.Sleep(delay);
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone iPhone");
+Smartphone iphone = new Iphone("654321", "Iphone", "222222", 64);
+iphone.Ligar();
+Thread.Sleep(delay);
+iphone.InstalarAplicativo("Youtube");
+Thread.Sleep(delay);
